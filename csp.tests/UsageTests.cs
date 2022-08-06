d406e06cf7e71d3adf01cae665d7b93f554766f6
Joshua Jensch <joshua@jensch.dev>
2022-08-06 12:58:05 +0200

    move SendMoreMoney test to STR2 file, fail tests before infinite loop

--- csp.tests/UsageTests.cs
+++ csp.tests/UsageTests.cs
@@ -1,12 +1,11 @@
 using System;
-
 using System.Collections.Generic;
 using System.Linq;
 
 using Xunit;
 using Xunit.Abstractions;
 
-namespace csp.tests; 
+namespace csp.tests;
 
 public class UsageTests {
 	private readonly ITestOutputHelper _testOutputHelper;
@@ -29,8 +28,8 @@ public class UsageTests {
 		Assert.Single(problem.Constraints);
 
 		var assignment = new Assignment(new Dictionary<IVariable, object> {
-			{a, 1},
-			{b, -5}
+			{ a, 1 },
+			{ b, -5 }
 		});
 
 		Assert.True(assignment.IsCompleteFor(problem));
@@ -57,9 +56,9 @@ public class UsageTests {
 
 		var problem = prob.Build();
 
-		var ass = new Assignment(new Dictionary<IVariable, object>{
-			{x, 4},
-			{name, "Hans"}
+		var ass = new Assignment(new Dictionary<IVariable, object> {
+			{ x, 4 },
+			{ name, "Hans" }
 		});
 
 		Assert.True(problem.IsSatisfiedBy(ass));
@@ -87,69 +86,6 @@ public class UsageTests {
 		Assert.Equal(4, solutions[1].Values[b]);
 	}
 
-	[Fact]
-	public void SendMoreMoney() {
-		var prob = new ProblemBuilder();
-		var domain = Enumerable.Range(0, 10);
-
-		var vars = "send more money".ToCharArray()
-			.Distinct()
-			.Where(x => x != ' ')
-			.SelectVariableTable(c => prob.AddVariable(domain, $"'{c}'"));
-		var carries = Enumerable.Range(0, 4).Select(i => prob.AddVariable<int>(Enumerable.Range(0, 10))).ToList();
-
-		prob.AddConstraint(new AllDiffConstraint<int>(vars.Values.ToArray()).SetScope(vars.Values.ToArray()));
-		prob.AddConstraint(
-			(from d in vars['d']
-				from e in vars['e']
-				from y in vars['y']
-				from c0 in carries[0]
-				select d + e == (10 * c0) + y)
-			.SetScope(vars['d'], vars['e'], vars['y'], carries[0])
-		);
-		prob.AddConstraint(
-			(from n in vars['n']
-				from r in vars['r']
-				from e in vars['e']
-				from c0 in carries[0]
-				from c1 in carries[1]
-				select n + r + c0 == (10 * c1) + e)
-			.SetScope(vars['n'], vars['r'], vars['e'], carries[0], carries[1])
-		);
-		prob.AddConstraint(
-			(from e in vars['e']
-				from o in vars['o']
-				from n in vars['n']
-				from c1 in carries[1]
-				from c2 in carries[2]
-				select e + o + c1 == (10 * c2) + n)
-			.SetScope(vars['e'], vars['o'], vars['n'], carries[1], carries[2])
-		);
-		prob.AddConstraint(
-			(from s in vars['s']
-				from m in vars['m']
-				from o in vars['o']
-				from c2 in carries[2]
-				from c3 in carries[3]
-				select s + m + c2 == (10 * c3) + o)
-			.SetScope(vars['s'], vars['m'], vars['o'], carries[2], carries[3])
-
-		);
-		prob.AddConstraint(
-			(from c3 in carries[3] from m in vars['m'] select c3 == m)
-			.SetScope(carries[3], vars['m'])
-		);
-
-		var problem = prob.Build();
-
-		var solver = new STR2Solver(problem);
-
-		foreach (var solution in solver.GetSolutions()) {
-			Assert.NotNull(solution);
-			// TODO: verify correctness
-			_testOutputHelper.WriteLine(solution.Assignment.ToString());
-		}
-	}
 
 	private enum Color {
 		Red,
@@ -168,7 +104,7 @@ public class UsageTests {
 		var victoria = prob.AddVariable(Enum.GetValues<Color>(), "Victoria");
 		var _ = prob.AddVariable(Enum.GetValues<Color>(), "Tansania");
 
-		var neighbours = new[]{
+		var neighbours = new[] {
 			(west, north),
 			(west, south),
 			(north, south),
